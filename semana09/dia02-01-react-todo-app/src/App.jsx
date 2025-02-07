a61af07bb0bb74aa51a478ac8cb1caac8ffb80fe
Esteban <esteban2029@gmail.com>
2025-02-06 23:32:33 -0500

    comit 5

--- semana09/dia02-01-react-todo-app/src/App.jsx
+++ semana09/dia02-01-react-todo-app/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import TodoHeader from "./components/TodoHeader"
+import TodoList from "./components/TodoList"
 
 const App = () => {
   const DEFAULT_TODOS = [
@@ -21,6 +22,7 @@ const App = () => {
   ]
 
   const [todos, setTodos] = useState(DEFAULT_TODOS)
+  const [input, setInput ] = useState('')
 
   console.log(todos)
 
@@ -45,34 +47,59 @@ const App = () => {
     setTodos(updatedTodos)
   }
 
+  const handleRemoveTodo = (event) => {
+    const { id } = event.target.dataset
+
+    const updatedTodos = todos.filter(todo => todo.id !== id)
+
+    setTodos(updatedTodos)
+  }
+
+  const handleChange = (event) => {
+    // Vamos a capturar lo que escribimos en la caja de texto
+    setInput(event.target.value)
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+    const newTodo = {
+      id: crypto.randomUUID(),
+      title: input,
+      completed: false
+    }
+
+    console.log(newTodo)
+
+    setTodos([...todos, newTodo])
+
+    setInput('')
+  }
+ 
   return (
     <main
       className="bg-yellow-100 w-[400px] mx-auto mt-10 border border-yellow-400 rounded-lg shadow-lg p-4"
     >
       <TodoHeader title='TODO APP + React + Tailwind' color='text-blue-500' />
 
-      <ul className="flex flex-col gap-2 mt-4">
-        {todos.map(todo => {
-          return (
-            <li key={todo.id} className="flex gap-2 bg-yellow-200 p-2 rounded-lg">
-              <input
-                type="checkbox"
-                data-id={todo.id}
-                checked={todo.completed}
-                onChange={handleCompleted}
-              />
-              <div className="w-full flex justify-between items-center">
-                <span className={`font-medium ${todo.completed ? 'line-through' : ''}`}>
-                  {todo.title}
-                </span>
-                <button className="bg-red-300 rounded-lg px-1 py-1 cursor-pointer">
-                  ❌
-                </button>
-              </div>
-            </li>
-          )
-        })}
-      </ul>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="w-full border border-yellow-400 my-3 p-2 rounded-lg"
+          placeholder="¿Qué deseas hacer hoy?"
+          required
+          value={input}
+          onChange={handleChange}
+        />
+
+        {/* {input} */}
+      </form>
+
+      <TodoList
+        todos={todos}
+        onCompleted={handleCompleted}
+        onRemoveTodo={handleRemoveTodo}
+      />
 
       <pre className="bg-slate-100 p-4 mt-8">{JSON.stringify(todos, null, 2)}</pre>
     </main>
