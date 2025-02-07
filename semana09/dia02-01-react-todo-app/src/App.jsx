4ef7bf4f3fb98cc2132db8001930e20576874b74
Esteban <esteban2029@gmail.com>
2025-02-06 20:19:47 -0500

    mqauetando01

--- semana09/dia02-01-react-todo-app/src/App.jsx
+++ semana09/dia02-01-react-todo-app/src/App.jsx
@@ -1,17 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+import TodoHeader from "./components/TodoHeader"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const DEFAULT_TODOS = [
+    {
+      id: '1',
+      title: 'Aprender Javascript',
+      completed: true
+    },
+    {
+      id: '2',
+      title: 'Aprender CSS',
+      completed: false
+    },
+    {
+      id: '3',
+      title: 'Aprender React.js + Tailwind',
+      completed: true
+    }
+  ]
+
+  const [todos, setTodos] = useState(DEFAULT_TODOS)
+
+  console.log(todos)
 
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    </>
+    <main
+      className="bg-yellow-100 w-[400px] mx-auto mt-10 border border-yellow-400 rounded-lg shadow-lg p-4"
+    >
+      <TodoHeader title='TODO APP + React + Tailwind' color='text-blue-500' />
+
+      <ul>
+        {todos.map(todo => {
+          return (
+            <li key={todo.id}>{todo.title}</li>
+          )
+        })}
+      </ul>
+
+      <pre className="bg-slate-100 p-4 mt-8">{JSON.stringify(todos, null, 2)}</pre>
+    </main>
   )
 }
 
